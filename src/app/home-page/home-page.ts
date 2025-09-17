40c074cea1b5f63f723303b5da9c36449dc6f707
vedantishiurkar <vedantishiurkar@gmail.com>
2025-09-17 20:33:18 +0530

    project_commit

---
+++ src/app/home-page/home-page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-home-page',
+  standalone:true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+ 
+
+}
