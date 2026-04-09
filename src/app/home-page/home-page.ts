3f41509fb6f4bc2982070cae42a312d440189c0c
vedantishiurkar <vedantishiurkar@gmail.com>
2026-04-09 20:18:11 +0530

    slider_implementation

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'
 
@@ -9,7 +9,43 @@ import { CommonModule } from '@angular/common'
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
-export class HomeComponent {
- 
+export class HomeComponent implements OnInit , OnDestroy {
+ images = [
+    'assets/robot1.jpeg',
+    'assets/robot2.jpeg',
+    'assets/robot3.jpeg'
+  ];
 
+  image: string = 'assets/robot1.jpeg';
+
+  currentIndex = 0;
+  intervalId: any;
+  
+
+  ngOnInit() {
+    // this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.next();
+    }, 3000);
+  }
+
+   
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId); // prevent memory leak
+  }
 }
+
+
